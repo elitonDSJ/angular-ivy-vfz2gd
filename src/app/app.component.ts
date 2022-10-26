22c193db6dd8457a7609a7abe3e82029dae62289
elitonDSJ <50776515+elitonDSJ@users.noreply.github.com>
2022-10-25 22:03:11 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public titulo: string = 'Hello World';
+  public nome: string = '';
+  public sobrenome: string = '';
+  public a: number = 0;
+  public anoNascimento: number = 0;
+  public anoAtual: number = 2022;
+  salvar() {
+    console.log('|-> ' + this.sobrenome + ' ' + this.nome);
+  }
+
+  calc() {
+    this.a = this.anoAtual - this.anoNascimento;
+    console.log(this.a);
+    console.log('Idade: ' + (this.anoAtual - this.anoNascimento));
+  }
 }
